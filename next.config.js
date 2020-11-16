1e8d7969854261af63dcbd2d6c34a8c46c8f0b71
Sean Norwood <norwood.sean@gmail.com>
2020-11-15 19:41:19 -0600

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const withOffline = require("next-offline");
+
+const nextConfig = {
   images: {
     domains: ["acnhapi.com"],
   },
 };
+
+module.exports = withOffline(nextConfig);
