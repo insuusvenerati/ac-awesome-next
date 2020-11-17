9a1f763abe7bfce6ef8abd7e5201c5cbae43f23e
Sean Norwood <norwood.sean@gmail.com>
2020-11-17 10:21:52 -0600

    chore: add sw config

--- next.config.js
+++ next.config.js
@@ -6,4 +6,35 @@ const nextConfig = {
   },
 };
 
-module.exports = withOffline(nextConfig);
+const swConfig = {
+  images: {
+    domains: ["acnhapi.com"],
+  },
+  target: "serverless",
+  transformManifest: (manifest) => ["/"].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: false,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = withOffline(swConfig);
